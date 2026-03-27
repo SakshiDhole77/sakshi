8164c08b2c1024f66a98d0922fa81936dc042154
SakshiDhole77 <dholesakshi45@gmail.com>
2026-03-27 13:54:59 +0530

    Add files via upload

---
+++ fibonachi series.cpp
@@ -0,0 +1,24 @@
+
+
+#include <stdio.h>
+ main(){
+	int n,first=0,second=1,next,i;
+	printf("enter the number of term:");
+	scanf("%d",&n);
+	printf("fibonachi series:");
+	 for(i=0; i<n; i++)
+	 {
+	 	if(i<=1)
+		 {
+	 		    next=i;
+         }
+		 else
+		 {
+		 	next=first+second;
+		 	first=second;
+		 	second=next;
+		 }
+		 printf("%d\t",next);
+	 }
+	 return 0;
+}
